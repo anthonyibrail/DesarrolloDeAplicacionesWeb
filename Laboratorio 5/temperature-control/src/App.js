b2e60d6f4a53d1738c272ad7a6d6e9f183e01809
Anthony Ibrail Ortega Méndez <anthony.ibrail@gmail.com>
2022-07-13 21:52:08 -0500

    Update App.js

--- Laboratorio 5/temperature-control/src/App.js
+++ Laboratorio 5/temperature-control/src/App.js
@@ -1,17 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
 // 1) Reemplaza estas variables por useState, puedes utilizar los valores iniciales que ya traen las variables
-  let temperatureColor = 'cold'
-  let temperatureValue = '10'
-  
+  //let temperatureColor = 'cold'
+  //let temperatureValue = '10'
+  const [temperatureColor, settemperatureColor] = useState("cold")
+  const [temperatureValue, settemperatureValue] = useState(10)
+ 
 // 2) Agregar las funciones para manejar los eventos de Click a los botones para que 
 //     cuando se presionen se incremente o decremente el valor de la temperatura. 
+  const aumentarTemperatura =() =>{
+    //Si dejaba el 20 el color cambiaba cuando se mostraba el 22°C
+    (temperatureValue >= 19) ? settemperatureColor("hot") : settemperatureColor("cold");
+
+    if (temperatureValue < 30) settemperatureValue(temperatureValue + 1)
+  } 
+
+  const decrementarTemperatura =() =>{
+    //Si dejaba el 20 el color cambiaba hasta que se mostrara el 18°C
+    (temperatureValue < 21) ? settemperatureColor("cold") : settemperatureColor("hot")
+
+    if (temperatureValue > 0) settemperatureValue(temperatureValue - 1)
+  } 
 
 // 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
 //    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
-
+  
 // Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
 // De igual manera al llegar los valores a 0 no se podrá decrementas más.
 
@@ -23,8 +39,8 @@ function App() {
         </div>
       </div>
       <div className="button-container">
-        <button >+</button>
-        <button >-</button>
+        <button onClick={aumentarTemperatura}>+</button>
+        <button onClick={decrementarTemperatura}>-</button>
       </div>
     </div>
   );
